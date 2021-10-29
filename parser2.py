d7c046637e35f121c3e6ebf79f8051fe99db5d68
Your Name <armennmuradyan@gmail.com>
2021-10-29 22:19:21 +0400

    parser homework

---
+++ parser2.py
@@ -0,0 +1,32 @@
+import yaml
+import json
+
+# json to text
+
+# with open("parser2.json", "r") as json_file:
+#     data = json.load(json_file)
+#     str_data = json.dumps(data)
+#     with open("parser2.text", "w") as text_file:
+#         text_file.write(str_data)
+
+# json to yaml
+
+# with open("parser2.json", "r") as json_file:
+#     data = json.load(json_file)
+#     with open("parser2.yaml", "a") as yaml_file:
+#         res = yaml.dump(data, yaml_file)
+
+# yaml to json
+
+# with open("parser2.yaml", "r") as yaml_file:
+#     data = yaml.safe_load(yaml_file)
+#     with open("parser2.json", "a") as json_file:
+#         json.dump(data, json_file)
+
+#yaml to text
+
+# with open("parser2.yaml", "r") as yaml_file:
+#     data = yaml.safe_load(yaml_file)
+#     str_data = json.dumps(data)
+#     with open("parser2.text", "w") as text_file:
+#         text_file.write(str_data)
